908394facb1a40784e73eb4bffd49b4d7d77c2d7
Joachim Hofer <jmhofer.github@johoop.de>
2018-03-16 13:43:23 +0100

    add a basic display helper

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 pub mod row4;
 
 use row4::Board;
+use row4::Color;
 
 fn main() {
-    println!("Hello, world!");
-
-    let board = Board::new();
+    let mut board = Board::new();
     println!("{:?}", board.moves());
-    println!("{:?}", board);
+    println!("{}{:?}", board, board);
+
+    board = board.play_move(Color::Red, 3);
+    println!("{}{:?}", board, board);
+
+    board = board.play_move(Color::Blue, 3);
+    println!("{}{:?}", board, board);
+
+    board = board.play_move(Color::Red, 4);
+    println!("{}{:?}", board, board);
 }
--- src/row4.rs
+++ src/row4.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub type Column = u8;
 
 pub enum Color {
@@ -11,7 +13,7 @@ pub const COLUMNS: [Column; 7] = [3, 2, 4, 1, 5, 0, 6];
 ///
 /// Bits 0..6 of the first byte are used as the lowest row of the board,
 /// bits 0..6 of the second byte as the second-lowest row, and so on.
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct Board {
     red: u64,
     blue: u64,
@@ -19,14 +21,17 @@ pub struct Board {
 }
 
 impl Board {
+    /// creates an empty board
     pub fn new() -> Board {
         Board { red: 0, blue: 0, column_heights: [0; 7] }
     }
 
+    /// utility to complete a bit mask from a given position
     pub fn position_mask(column: Column, height: u8) -> u64 {
         1u64 << (height * 8 + column)
     }
 
+    /// play a move in the specified column
     /// does not check if the column is legal - this has to be done beforehand!
     pub fn play_move(&self, color: Color, column: Column) -> Board {
         let height = self.height(column);
@@ -46,7 +51,7 @@ impl Board {
     /// computes all currently available moves for this board
     pub fn moves(&self) -> Vec<Column> {
         let mut moves = Vec::new();
-        for i in 0usize .. 7 {
+        for i in 0..7usize {
             let column = COLUMNS[i];
             if self.height(column) < 6 {
                 moves.push(column);
@@ -55,8 +60,28 @@ impl Board {
         moves
     }
 
+    /// returns the height of the specified column
     pub fn height(&self, column: Column) -> u8 {
         self.column_heights[column as usize]
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, dest: &mut fmt::Formatter) -> fmt::Result {
+        for row in 0..6u8 {
+            for column in 0..7u8 {
+                let mask = Board::position_mask(column, 5 - row);
+                if self.red & mask != 0 {
+                    write!(dest, "x ")?;
+                } else if self.blue & mask != 0 {
+                    write!(dest, "o ")?;
+                } else {
+                    write!(dest, ". ")?;
+                }
+            }
+            write!(dest, "\n")?
+        }
+        Ok(())
     }
 }
 
